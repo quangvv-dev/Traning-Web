efe5037d5e801a038c98ac0811b3de9aa3855504
bibixu <quangphuong9685@gmail.com>
2018-07-03 10:55:18 +0700

    quangvv update

--- Quangvv-Responsive-web/resources/views/welcome.blade.php
+++ Quangvv-Responsive-web/resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="title" content="Tot cho seo"/>
     <meta name="description" content="Seo Web tot nhat VN"/>
     <title>Laravel</title>
@@ -17,33 +18,50 @@
     <link rel="stylesheet" href="css/PageCenterBelow.css">
 </head>
 <body>
-<div class="background">
+
     <div class="container-full">
         {{--Header Web--}}
         <header>
-
-
             <div class="container-header-logo">
                 <img src="image/logo.png" alt="logo-vinsofts">
             </div>
 
             <div class="container-header-menu">
-
                 <div class="container-header-menu-info">
-                    <img src="image/icon_skype.png" alt="icon-skype"/>&nbsp;Skype:Vinsofts.com&emsp;&emsp;
-                    <img src="image/icon_email.png" alt="icon-email" style="margin-top:-3px;"/>&nbsp;Mail:Contact@vinsofts.com&emsp;&emsp;
-                    <img src="image/icon_phone.png" alt="icon-phone"/>&nbsp; Hotline:0988.988.988
+                    <img src="image/icon_skype.png" class="icon" alt="icon-skype"/>&nbsp;Skype:Vinsofts.com&emsp;&emsp;
+                    <img src="image/icon_email.png" class="icon" alt="icon-email" style="margin-top:-3px;"/>&nbsp;Mail:Contact@vinsofts.com&emsp;&emsp;
+                    <img src="image/icon_phone.png" class="icon" alt="icon-phone"/>&nbsp; Hotline:0988.988.988
                 </div>
+                    <nav class="navbar navbar-expand-lg navbar-light">
 
-                <nav class="container-header-menu-navbar">
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Protfolio</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Gallery</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Recruitment</a></li>
-                    </ul>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Protfolio</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contact</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Recruitment </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                     <div class="container-header-menu-icon" >
                         <a href="http://www.facebook.com/"><img src="image/dssd_12.png" alt="Facebook" width="48" style="text-decoration: none; width: 30px;"></a>
                         <a href="https://twitter.com/"><img src="image/dssd_14.png" alt="Twitter" width="48" style="text-decoration: none; width: 30px;"></a>
@@ -51,18 +69,15 @@
                         <a href="http://convertkit.com"><img src="image/icon_gg.png" alt="Website" width="48" style="text-decoration: none; width: 30px;"></a>
                     </div>
 
-                </nav>
-
 
             <div class="clear"></div>
             </div>
         </header>
-
         <div class="container-banner">
             <div class=" container-banner-line">
                 <h5 class="container-banner-text-image">WEB DESIGN COMPANY REPUPATION</h5>
-                <p>we've been brougth the most valued products and
-                    servecies to our global clients during last 10 year.We are experts in mobile development industry</p></div>
+                <p class="content-text">we've been brougth the most valued products and
+                    servecies to our global clients during last 10 year.We are experts in mobile development in industry</p></div>
         </div>
         <div class="container-services">
         <div class="container-services-title">
@@ -80,10 +95,8 @@
                             skilled developers that have been in this
                             field for at least 5 years so far. </p>
                         <button type="submit"><span>Explore more &raquo</span></button>
-
                     </div>
                     <div class="col-sm">
-
                         <img src="image/hinh2.png" alt="logo-services"/>
                         <h5>Mobile App Developongment</h5>
                         <p>We are experts in mobile development
@@ -109,38 +122,38 @@
         <div class="container-client">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm"></div>
-                    <div class="col-sm">
+                    <div class="col-md col-sm-12"></div>
+                    <div class="col-md col-sm-12">
                         <div class=" container-client-text">
                             <h2>OURS CLIENTS</h2>
-                            <h6>The Circle Of Live And Deft Continue ...</h6>
+                            <h6>The Circle Of Live And Deft Continue..</h6>
                         </div>
                     </div>
-                    <div class="col-sm"></div>
+                    <div class="col-md col-sm-12"></div>
                 </div>
             </div>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm">
-                        <img src="image/logo2.png" alt="logo-client"/>
+                    <div class="col-md col-sm-12">
+                        <img src="image/logo2.png"  alt="logo-client"/>
                     </div>
-                    <div class="col-sm">
-                        <img src="image/logo2.png" alt="logo-client"/>
+                    <div class="col-md col-sm-12">
+                        <img src="image/logo2.png"  alt="logo-client"/>
                     </div>
-                    <div class="col-sm">
-                        <img src="image/logo2.png" alt="logo-client"/>
+                    <div class="col-md col-sm-12">
+                        <img src="image/logo2.png"  alt="logo-client"/>
                     </div>
-                    <div class="col-sm">
-                        <img src="image/logo2.png" alt="logo-client"/>
+                    <div class="col-md col-sm-12">
+                        <img src="image/logo2.png"  alt="logo-client"/>
                     </div>
-                    <div class="col-sm">
-                        <img src="image/logo2.png" alt="logo-client"/>
+                    <div class="col-md col-sm-12">
+                        <img src="image/logo2.png"  alt="logo-client"/>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm"></div>
-                    <div class="col-sm"><hr></div>
-                    <div class="col-sm"></div>
+                    <div class="col-md col-sm-12"></div>
+                    <div class="col-md col-sm-12"><hr></div>
+                    <div class="col-md col-sm-12"></div>
                 </div>
             </div>
         </div>
@@ -155,16 +168,14 @@
                         <div class="container-article-text-des">
                             <br>
                             <h5>Mobile App Developongment</h5>
-
                             <p>We are experts in mobile development
                                 in industry for years. We have a team with 10
                                 skilled developers that have been in this
                                 field for at least 5 years so far. </p></div></div>
-                    <div class="col-sm"> <img data-src="image/logo3-2.png" alt="logo-article-2" />
+                    <div class="col-sm"><img data-src="image/logo3-2.png" alt="logo-article-2" />
                         <div class="container-article-text-des">
                             <br>
                             <h5>Mobile App Developongment</h5>
-
                             <p>We are experts in mobile development
                                 in industry for years. We have a team with 10
                                 skilled developers that have been in this
@@ -173,7 +184,6 @@
                         <div class="container-article-text-des">
                             <br>
                             <h5>Mobile App Developongment</h5>
-
                             <p>We are experts in mobile development
                                 in industry for years. We have a team with 10
                                 skilled developers that have been in this
@@ -214,11 +224,11 @@
             </div>
         </footer>
     </div>
-</div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="js/lazyload.min.js"></script>
-<script type="text/javascript">;
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <script src="js/lazyload.min.js"></script>
+<script type="text/javascript">
     new LazyLoad({
         elements_selector:["img","iframe"],
     });
